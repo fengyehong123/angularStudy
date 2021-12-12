aba466d020350c858503fa18a24bedeeedf03fd4
fengyehong123 <1355930128@qq.com>
2021-12-12 09:12:09 +0800

    add

--- angulardemo01/src/app/app.component.ts
+++ angulardemo01/src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
 
 @Component({
+  // 指定模板中的标签
   selector: 'app-root',
+  // 指定模板的位置
   templateUrl: './app.component.html',
+  // 指定模板中css样式
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
+  // 相当于vue中的data区域
   title = 'angulardemo01';
+  count = 0;
+
+  // 相当于vue中的method
+  increment = function() {
+    this.count++;
+  };
 }
--- angulardemo01/src/app/app.module.ts
+++ angulardemo01/src/app/app.module.ts
@@ -2,15 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { FooComponent } from './foo/foo.component';
 
 @NgModule({
+  /**
+   * 声明组件模块资源: 组件,指令,服务
+   * 该模块中包含了多个组件
+   */
   declarations: [
-    AppComponent
+    AppComponent,
+    FooComponent
   ],
+  // 依赖模块
   imports: [
     BrowserModule
   ],
   providers: [],
+  // 指定启动根组件,指定以哪个组件为入口
   bootstrap: [AppComponent]
 })
+// 将模块导出
 export class AppModule { }
