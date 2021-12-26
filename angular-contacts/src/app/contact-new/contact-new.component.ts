d688f01f33d18330ea78c8f157400490152b7f41
fengyehong123 <1355930128@qq.com>
2021-12-26 12:48:37 +0800

    update

--- angular-contacts/src/app/contact-new/contact-new.component.ts
+++ angular-contacts/src/app/contact-new/contact-new.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+// 导入http请求Client
+import {HttpClient} from '@angular/common/http';
+// 导入路由
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-contact-new',
@@ -7,9 +11,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactNewComponent implements OnInit {
 
-  constructor() { }
+  // 表单对象
+  formData = {
+    name: '',
+    email: '',
+    phone: ''
+  };
+
+  constructor(
+    private http: HttpClient,
+    private router: Router
+  ) { }
 
   ngOnInit() {
   }
 
+  // 添加联系人到后台
+  addContact() {
+    this.http.post('http://localhost:3000/contacts', this.formData)
+    .toPromise().then((data: any) => {
+      // 联系人添加成功后,跳转到首页
+      this.router.navigate(['/contacts']);
+    }).catch(err => {
+      console.log(err);
+    });
+  }
 }
